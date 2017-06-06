ba19d9c8e77d2a74d148e191cb80ee39bf9cf07a
Ravindra <kmsravindra@yahoo.com>
2017-06-06 19:35:53 +0530

    initial version of titanic dataset prediction

---
+++ titanic.R
@@ -0,0 +1,208 @@
+rm(list=ls(all=TRUE))
+setwd(choose.dir())
+getwd()
+
+df = read.csv("train.csv", header = T)
+df_test = read.csv("test.csv", header = T)
+df_test = as.data.frame(append(df_test, list(Survived = 0), after = 1))
+
+df = rbind(df,df_test)
+rm(df_test)
+
+
+library(vegan)
+library(infotheo)
+library(DMwR)
+library(dummies)
+
+EmbarkedVars = dummy(df$Embarked)
+df = cbind(df,EmbarkedVars)
+df  = subset(df, select=-c(Embarked))
+
+ClassVars = dummy(df$Pclass)
+df = cbind(df,ClassVars)
+df  = subset(df, select=-c(Pclass))
+
+SexVars = dummy(df$Sex)
+df = cbind(df,SexVars)
+df  = subset(df, select=-c(Sex))
+names(df)
+str(df)
+colnames(df)
+
+num_attr = c("Age", "Fare", "SibSp", "Parch")
+cat_attr = setdiff(colnames(df), num_attr)
+df_num = df[,num_attr]
+df_cat = df[,cat_attr]
+df_cat = data.frame(apply(df_cat, MARGIN = 2, FUN = function(x) as.factor(as.character(x))))
+str(df_cat)
+df = cbind(df_cat,df_num)
+str(df)
+sum(is.na(df))
+
+unique(df$Survived)
+table(df$Survived)
+
+#df_omitNA = na.omit(df)
+#rm(df_omitNA)
+#summary(df)
+
+# extract the list of titles
+library(qdapRegex)
+MrTitle = c("Mr", "Don", "Major", "Capt", "Jonkheer", "Rev", "Col", "Sir")
+MrsTitle = c("Countess", "Mme", "Mrs")
+MissTitle = c("Mlle", "Ms", "Miss", "Lady", "the Countess")
+DrTitle = c("Dr")
+MasterTitle = c("Master")
+
+titles = sapply(df$Name, FUN = function(x) {
+  if (is.element(rm_between(x, ",", ".", extract=TRUE), MrTitle)) {
+    return("Mr") }
+  else if (is.element(rm_between(x, ",", ".", extract=TRUE), MrsTitle)) {
+    return("Mrs")} 
+  else if (is.element(rm_between(x, ",", ".", extract=TRUE), MissTitle)) {
+    return("Miss")}
+  else if (is.element(rm_between(x, ",", ".", extract=TRUE), MasterTitle)) {
+    return("Master")}
+  else if (is.element(rm_between(x, ",", ".", extract=TRUE), DrTitle)) {
+    return("Dr")}
+  else {
+    return("Ha")}
+})
+
+#titles = unlist(titles)
+
+df = cbind(df,titles)
+table(df$titles)
+
+TitlesVars = dummy(df$titles)
+df = cbind(df,TitlesVars)
+df  = subset(df, select=-c(titles))
+df  = subset(df, select=-c(titlesHa))
+str(df)
+
+df$titlesDr = as.factor(as.character(df$titlesDr))
+df$titlesMaster = as.factor(as.character(df$titlesMaster))
+df$titlesMiss = as.factor(as.character(df$titlesMiss))
+df$titlesMr = as.factor(as.character(df$titlesMr))
+df$titlesMrs = as.factor(as.character(df$titlesMrs))
+#df$titlesHa = as.factor(as.character(df$titlesHa))
+str(df)
+
+
+classInfo = sapply(df$Cabin, function(x) {
+  if(substr(x, 1, 1) != "") {
+    return(substr(x, 1, 1))
+  } else {
+    return ("U")
+  }
+}
+)
+
+table(classInfo)
+
+df = cbind(df,classInfo)
+classInfoVars = dummy(df$classInfo)
+df = cbind(df,classInfoVars)
+df  = subset(df, select=-c(classInfo))
+
+df$classInfoA = as.factor(as.character(df$classInfoA))
+df$classInfoB = as.factor(as.character(df$classInfoB))
+df$classInfoC = as.factor(as.character(df$classInfoC))
+df$classInfoD = as.factor(as.character(df$classInfoD))
+df$classInfoE = as.factor(as.character(df$classInfoE))
+df$classInfoF = as.factor(as.character(df$classInfoF))
+df$classInfoG = as.factor(as.character(df$classInfoG))
+df$classInfoT = as.factor(as.character(df$classInfoT))
+df$classInfoU = as.factor(as.character(df$classInfoU))
+
+str(df)
+sum(is.na(df))
+
+unique(df$Survived)
+df$PassengerId =  NULL
+
+df_imputed <- knnImputation(data = df,k=8) #KNN Imputation
+#df_imputed <- centralImputation(data = df) #Central Imputation
+
+# Standardizing
+num_attr = c("Age", "Fare", "SibSp", "Parch")
+cat_attr = setdiff(colnames(df), num_attr)
+
+df_imputed_num = df_imputed[,num_attr]
+df_imputed_cat = df_imputed[,cat_attr] 
+
+df_imputed_num1 = decostand(df_imputed_num, "standardize")
+
+df_final = cbind(df_imputed_num1,df_imputed_cat)
+rm(df_imputed_num1, df_imputed_num, df_imputed_cat)
+rm(df_num)
+rm(df_cat)
+sum(is.na(df_final))
+
+library(RColorBrewer)
+hist(df_final$Age)
+hist(df_final$Age,col=brewer.pal(12,"Set3"))
+boxplot(df_final$Age)
+barplot(table(df_final$Survived),col = brewer.pal(3,"Set3")) 
+
+plot(df_final$Fare, df_final$Age, xlab = "Fare", ylab = "Age" )
+boxplot(Age~Survived,data = df_final,xlab ="TARGET",ylab = "Age",main = "Continuous v/s Categorical")
+boxplot(Fare~Survived,data = df_final,xlab ="TARGET",ylab = "Fare",main = "Continuous v/s Categorical")
+
+df_final = subset(df_final, select=-c(Cabin))
+df_final = subset(df_final, select=-c(Ticket))
+df_final = subset(df_final, select=-c(Name))
+df_final = subset(df_final, select=-c(Embarked))
+
+
+trainData <- df_final[1:891,]
+test <- df_final[892:1309,]
+
+train = subset(train, select=-c(titlesHa))
+
+summary(train)
+
+(l <- sapply(train, function(x) is.factor(x)))
+m =  train[, l]
+ifelse(n <- sapply(m, function(x) length(levels(x))) == 1, "DROP", "NODROP")
+
+#model <- glm(Survived ~.,family=binomial(link='logit'),data=train)
+library(caret)
+library(e1071)
+#model = train(Survived~., data=trainData, method="svmRadial", preProc=c("BoxCox"), trControl=trainControl((method = "boot")))
+model <- svm(Survived ~ ., data = trainData)
+
+summary(model)
+
+
+test = subset(test, select = -c(titlesHa))
+
+fitted.results <- predict(model,newdata=test,type='response')
+fitted.results <- ifelse(fitted.results > 0.5,1,0)
+
+fitted.results = as.vector(fitted.results)
+
+fitted.results = data.frame(Survived= fitted.results)
+
+testData = read.csv("test.csv", header = T)
+
+myResults = cbind(testData, fitted.results)
+
+write.csv(myResults,file = "myResults6.csv")
+
+
+misClasificError <- mean(fitted.results != test$Survived)
+print(paste('Accuracy',1-misClasificError))
+
+library(gplots)
+library(ROCR)
+p <- predict(model, newdata=test, type="response")
+pr <- prediction(p, test$Survived)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+
